718fd7bfdc39f514767e8c9c1501e14369f126d0
Jonaxx8 <20r21a0543@mlrinstitutions.ac.in>
2023-08-25 13:38:18 +0530

    it is able to fetch pictures from the app

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const multer = require('multer');
+const fs = require('fs'); // Import the fs module
+const path = require('path'); // Import the path module
 const app = express();
 const port = 3000;
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post('/upload-image', upload.single('image'), (req, res) => {
+    const imageBuffer = req.file.buffer;
+    
+    // Save the image to a folder on the server
+    const imageName = 'uploaded-image.jpg'; // Choose a suitable name
+    const imagePath = path.join(__dirname, 'images', imageName);
+    fs.writeFileSync(imagePath, imageBuffer);
+    
+    // Respond with a success message
+    res.json({ message: 'Image uploaded successfully' });
+});
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
